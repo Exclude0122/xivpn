610324b8b453a048c63d78b32f55ce958a106291
Exclude0122 <173652916+Exclude0122@users.noreply.github.com>
2026-06-23 07:28:13 -0400

    remove about section

--- app/src/main/java/cn/gov/xivpn2/ui/SettingsFragment.java
+++ app/src/main/java/cn/gov/xivpn2/ui/SettingsFragment.java
@@ -27,32 +27,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
-        findPreference("feedback").setOnPreferenceClickListener(preference -> {
-            openUrl("https://github.com/Exclude0122/xivpn/issues/new");
-            return true;
-        });
-
-        findPreference("privacy_policy").setOnPreferenceClickListener(preference -> {
-            openUrl("https://exclude0122.github.io/docs/privacy-policy.html");
-            return true;
-        });
-
-        findPreference("source_code").setOnPreferenceClickListener(preference -> {
-            openUrl("https://github.com/Exclude0122/xivpn");
-            return true;
-        });
-
-        findPreference("open_source_licenses").setOnPreferenceClickListener(preference -> {
-            startActivity(new Intent(requireContext(), LicensesActivity.class));
-            return true;
-        });
 
         findPreference("black_background").setOnPreferenceChangeListener((preference, newValue) -> {
             Toast.makeText(getContext(), R.string.restart_to_apply, Toast.LENGTH_SHORT).show();
             return true;
         });
-
-        findPreference("app_version").setSummary(BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")");
 
         findPreference("geoip_geosite").setOnPreferenceClickListener(preference -> {
             startActivity(new Intent(getContext(), GeoAssetsActivity.class));
@@ -69,44 +48,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             return true;
         });
 
-        findPreference("donation").setOnPreferenceClickListener(preference -> {
-            new AlertDialog.Builder(requireContext())
-                    .setTitle(R.string.donation)
-                    .setItems(R.array.donation, (dialog, which) -> {
-                        String walletAddress = "";
-                        switch (which) {
-                            case 0: walletAddress = "TTpzvVJ7cv2RZVihd48GGZXg1896WFgQuJ"; break;
-                            case 1: walletAddress = "0x593065aDE108505356abaD9c58bE950115678593"; break;
-                            case 2: walletAddress = "CyRPKfkGnrAtVKijorcFYocL5fY37tX9j1atm2m8cY8m"; break;
-                            case 3: walletAddress = "84iR4Tz29wFKxDpceeFhZQc3msh7N59PdNqxhEY9HjtZKs7wHqGLhw5AJ5p5zkxHMpU7DKHmhjjHmV7jaoVteoWsQs81tf3"; break;
-                            case 4: openUrl("https://github.com/sponsors/Exclude0122"); return;
-                        }
-
-                        String finalWalletAddress = walletAddress;
-                        new AlertDialog.Builder(requireContext())
-                                .setTitle(R.string.wallet_address)
-                                .setMessage(walletAddress)
-                                .setPositiveButton(R.string.copy, (dialog1, which1) -> {
-                                    ClipboardManager clipboardManager = requireContext().getSystemService(ClipboardManager.class);
-                                    clipboardManager.setPrimaryClip(ClipData.newPlainText("Wallet Address", finalWalletAddress));
-                                })
-                                .show();
-                    })
-                    .show();
-            return true;
-        });
     }
 
-    private void openUrl(String url) {
-        try {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(browserIntent);
-        } catch (ActivityNotFoundException e) {
-            Log.e("SettingsFragment", "open browser", e);
-        }
-    }
-
-    public int dp2px(float dp) {
-        return (int) (dp * ((float) requireContext().getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT));
-    }
 }
